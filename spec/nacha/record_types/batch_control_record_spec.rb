d303d7c95ce38f5d2248996cac6107ca7c59b395
David H. Wilkins <dwilkins@conecuh.com>
2017-03-27 21:55:08 -0500

    Add some batch tests
    - unit tests for batch_header and batch_control records

--- spec/nacha/record_types/batch_control_record_spec.rb
+++ spec/nacha/record_types/batch_control_record_spec.rb
@@ -2,8 +2,69 @@ require 'spec_helper'
 
 RSpec.describe "Nacha::Record::BatchControlRecord", :nacha_record_type do
 
+  let(:example_batch_control_record) {
+    #         1         2         3         4         5         6         7         8         9
+    #1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+    "822000000200248602040000000000000000002001002870327243                         124000050000001"
+  }
+
+
   it 'exists' do
     expect { Nacha::Record::BatchControlRecord }.to_not raise_error()
   end
 
+  it 'generates a valid unpack string' do
+    expect(Nacha::Record::BatchControlRecord.unpack_str).to eq 'A1A3A6A10A12A12A10A19A6A8A7'
+  end
+
+  it 'generates a regexp matcher' do
+    expect(Nacha::Record::BatchControlRecord.matcher).to be_a Regexp
+  end
+
+  it 'recognizes input' do
+    expect(Nacha::Record::BatchControlRecord.matcher).to match example_batch_control_record
+  end
+  describe 'parses a record' do
+    let(:bcr) { Nacha::Record::BatchControlRecord.parse(example_batch_control_record) }
+
+    it 'record_type_code' do
+      expect(bcr.record_type_code.to_ach).to eq '8'
+    end
+
+    it 'service_class_code' do
+      expect(bcr.service_class_code.to_ach).to eq '220'
+    end
+
+    it 'entry_addenda_count' do
+      expect(bcr.entry_addenda_count.to_ach).to eq '000002'
+    end
+
+    it 'entry_hash' do
+      expect(bcr.entry_hash.to_ach).to eq '0024860204'
+    end
+
+    it 'total_debit_entry_dollar_amount' do
+      expect(bcr.total_debit_entry_dollar_amount.to_ach).to eq '000000000000'
+    end
+
+    it 'total_credit_entry_dollar_amount' do
+      expect(bcr.total_credit_entry_dollar_amount.to_ach).to eq '000000200100'
+    end
+
+    it 'company_identification' do
+      expect(bcr.company_identification.to_ach).to eq '2870327243'
+    end
+
+    it 'message_authentication_code' do
+      expect(bcr.message_authentication_code.to_ach).to eq '                   '
+    end
+
+    it 'originating_dfi_identification' do
+      expect(bcr.originating_dfi_identification.to_ach).to eq '12400005'
+    end
+
+    it 'batch_number' do
+      expect(bcr.batch_number.to_ach).to eq '0000001'
+    end
+  end
 end
--- spec/nacha/record_types/batch_header_record_spec.rb
+++ spec/nacha/record_types/batch_header_record_spec.rb
@@ -2,8 +2,82 @@ require 'spec_helper'
 
 RSpec.describe "Nacha::Record::BatchHeaderRecord", :nacha_record_type do
 
+  let(:example_batch_header_record) {
+    #         1         2         3         4         5         6         7         8         9
+    #1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+    "5220DHI PAYROLL                         2870327243PPDDHIPAYROLL090702081205   1124000050000001"
+  }
+
   it 'exists' do
     expect { Nacha::Record::BatchHeaderRecord }.to_not raise_error()
+  end
+
+  it 'generates a valid unpack string' do
+    expect(Nacha::Record::BatchHeaderRecord.unpack_str).to eq 'A1A3A16A20A10A3A10A6A6A3A1A8A7'
+  end
+
+  it 'generates a regexp matcher' do
+    expect(Nacha::Record::BatchHeaderRecord.matcher).to be_a Regexp
+  end
+
+  it 'recognizes input' do
+    expect(Nacha::Record::BatchHeaderRecord.matcher).to match example_batch_header_record
+  end
+  describe 'parses a record' do
+    let(:record) { Nacha::Record::BatchHeaderRecord.parse(example_batch_header_record) }
+
+    it 'record_type_code' do
+      expect(record.record_type_code.to_ach).to eq '5'
+    end
+
+    it 'service_class_code' do
+      expect(record.service_class_code.to_ach).to eq '220'
+    end
+
+    it 'company_name' do
+      expect(record.company_name.to_ach).to eq 'DHI PAYROLL     '
+    end
+
+    it 'company_discretionary_data' do
+      expect(record.company_discretionary_data.to_ach).to eq '                    '
+    end
+
+    it 'company_identification' do
+      expect(record.company_identification.to_ach).to eq '2870327243'
+    end
+
+    it 'standard_entry_class_code' do
+      expect(record.standard_entry_class_code.to_ach).to eq 'PPD'
+    end
+
+    it 'company_entry_description' do
+      expect(record.company_entry_description.to_ach).to eq 'DHIPAYROLL'
+    end
+
+    it 'company_descriptive_date' do
+      expect(record.company_descriptive_date.to_ach).to eq '090702'
+    end
+
+    it 'effective_entry_date' do
+      expect(record.effective_entry_date.to_ach).to eq '081205'
+    end
+
+    xit 'settlement_date_julian' do
+      expect(record.settlement_date_julian.to_ach).to eq '000'
+    end
+
+    it 'originator_status_code' do
+      expect(record.originator_status_code.to_ach).to eq '1'
+    end
+
+    it 'originating_dfi_identification' do
+      expect(record.originating_dfi_identification.to_ach).to eq '12400005'
+    end
+
+    it 'batch_number' do
+      expect(record.batch_number.to_ach).to eq '0000001'
+    end
+
   end
 
 end
