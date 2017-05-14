87aa14d64078697d97d8646575f7490c3a7e9483
David H. Wilkins <dwilkins@conecuh.com>
2017-05-13 19:07:48 -0500

    Added more definitions
    - adv_file_header and adv_file_control

--- spec/nacha/record_types/adv_file_control_spec.rb
+++ spec/nacha/record_types/adv_file_control_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "Nacha::Record::AdvFileControl", :nacha_record_type do
 
   it 'exists' do
-    skip 'Need implementation'
     expect { Nacha::Record::AdvFileControl }.to_not raise_error()
   end
 
