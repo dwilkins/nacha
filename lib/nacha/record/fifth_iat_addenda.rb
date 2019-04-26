a0d658d6280d2c04230737498fd81f792c1ccafc
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 18:10:44 -0500

    Moving away from yml definition - arc_entry_detail

--- lib/nacha/record/fifth_iat_addenda.rb
+++ lib/nacha/record/fifth_iat_addenda.rb
@@ -4,24 +4,14 @@ module Nacha
   module Record
     class FifthIatAddenda < Nacha::Record::Base
       include AddendaRecordType
-      RECORD_DEFINITION = {
-        record_type_code:
-          {inclusion: "M", contents: "C7", position: 1..1},
-        addenda_type_code:
-          {inclusion: "M", contents: "C14", position: 2..3},
-        receiving_dfi_name:
-          {inclusion: "M", contents: "Alphameric", position: 4..38},
-        receiving_dfi_identification_number_qualifier:
-          {inclusion: "M", contents: "Alphameric", position: 39..40},
-        receiving_dfi_identification:
-          {inclusion: "M", contents: "Alphameric", position: 41..74},
-        receiving_dfi_branch_country_code:
-          {inclusion: "M", contents: "Alphameric", position: 75..77},
-        reserved:
-          {inclusion: "M", contents: "C          ", position: 78..87},
-        entry_detail_sequence_number:
-          {inclusion: "M", contents: "Numeric", position: 88..94}
-      }
+      nacha_field :record_type_code, inclusion: 'M', contents: 'C7', position: 1..1
+      nacha_field :addenda_type_code, inclusion: 'M', contents: 'C14', position: 2..3
+      nacha_field :receiving_dfi_name, inclusion: 'M', contents: 'Alphameric', position: 4..38
+      nacha_field :receiving_dfi_identification_number_qualifier, inclusion: 'M', contents: 'Alphameric', position: 39..40
+      nacha_field :receiving_dfi_identification, inclusion: 'M', contents: 'Alphameric', position: 41..74
+      nacha_field :receiving_dfi_branch_country_code, inclusion: 'M', contents: 'Alphameric', position: 75..77
+      nacha_field :reserved, inclusion: 'M', contents: 'C          ', position: 78..87
+      nacha_field :entry_detail_sequence_number, inclusion: 'M', contents: 'Numeric', position: 88..94
     end
   end
 end
