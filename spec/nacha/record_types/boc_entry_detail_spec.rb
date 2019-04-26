a90feb8184e6f299bd85f12cdf3c51369ed7b3e4
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 18:55:25 -0500

    Moving away from yml - boc_entry_detail

--- spec/nacha/record_types/boc_entry_detail_spec.rb
+++ spec/nacha/record_types/boc_entry_detail_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "Nacha::Record::BocEntryDetail", :nacha_record_type do
 
   it 'exists' do
-    skip 'Need implementation'
     expect { Nacha::Record::BocEntryDetail }.to_not raise_error()
   end
 
