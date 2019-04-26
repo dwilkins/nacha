ec08c61187d6b7973bfa3951cb300385072208c9
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 17:48:47 -0500

    Moving away from yml
    - Added adv_entry_detail

--- spec/nacha/record_types/adv_entry_detail_spec.rb
+++ spec/nacha/record_types/adv_entry_detail_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "Nacha::Record::AdvEntryDetail", :nacha_record_type do
 
   it 'exists' do
-    skip 'Need implementation'
     expect { Nacha::Record::AdvEntryDetail }.to_not raise_error()
   end
 
