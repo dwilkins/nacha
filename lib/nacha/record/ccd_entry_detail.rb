a8653e2659f09793dfce24e4657bee01d083272d
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 19:03:00 -0500

    Moving away from yml - ccd_entry_detail

--- lib/nacha/record/ccd_entry_detail.rb
+++ lib/nacha/record/ccd_entry_detail.rb
@@ -4,15 +4,15 @@ require 'nacha/record/detail_record_type.rb'
 
 module Nacha
   module Record
-    class BocEntryDetail < Nacha::Record::Base
+    class CcdEntryDetail < Nacha::Record::Base
       include DetailRecordType
       nacha_field :record_type_code, inclusion: 'M', contents: 'C6', position: 1..1
       nacha_field :transaction_code, inclusion: 'M', contents: 'Numeric', position: 2..3
       nacha_field :receiving_dfi_identification, inclusion: 'M', contents: 'TTTTAAAAC', position: 4..12
       nacha_field :dfi_account_number, inclusion: 'R', contents: 'Alphameric', position: 13..29
       nacha_field :amount, inclusion: 'M', contents: '$$$$$$$$¢¢', position: 30..39
-      nacha_field :check_serial_number, inclusion: 'M', contents: 'Alphameric', position: 40..54
-      nacha_field :individual_name, inclusion: 'O', contents: 'Alphameric', position: 55..76
+      nacha_field :identification_number, inclusion: 'O', contents: 'Alphameric', position: 40..54
+      nacha_field :receiving_company_name, inclusion: 'R', contents: 'Alphameric', position: 55..76
       nacha_field :discretionary_data, inclusion: 'O', contents: 'Alphameric', position: 77..78
       nacha_field :addenda_record_indicator, inclusion: 'M', contents: 'Numeric', position: 79..79
       nacha_field :trace_number, inclusion: 'M', contents: 'Numeric', position: 80..94
--- spec/nacha/record_types/ccd_entry_detail_spec.rb
+++ spec/nacha/record_types/ccd_entry_detail_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "Nacha::Record::CcdEntryDetail", :nacha_record_type do
 
   it 'exists' do
-    skip 'Need implementation'
     expect { Nacha::Record::CcdEntryDetail }.to_not raise_error()
   end
 
