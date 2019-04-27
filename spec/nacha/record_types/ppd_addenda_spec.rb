8e2c7fa6af4497c029d9903b1efe40273e2eddb2
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 20:32:04 -0500

    Moving away from yml - ppd_addenda

--- spec/nacha/record_types/ppd_addenda_spec.rb
+++ spec/nacha/record_types/ppd_addenda_spec.rb
@@ -5,9 +5,4 @@ RSpec.describe "Nacha::Record::PpdAddenda", :nacha_record_type do
   it 'exists' do
     expect { Nacha::Record::PpdAddenda }.to_not raise_error()
   end
-
-  it 'knows it\'s name' do
-    expect(Nacha::Record::PpdAddenda.name).to eq 'ppd_addenda'
-  end
-
 end
