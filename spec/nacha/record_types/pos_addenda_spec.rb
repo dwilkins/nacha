dabf2eb35fe5437bbd37a96ac2fb500caf564da6
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 19:56:08 -0500

    Moving away from yml - pos_addenda

--- spec/nacha/record_types/pos_addenda_spec.rb
+++ spec/nacha/record_types/pos_addenda_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "Nacha::Record::PosAddenda", :nacha_record_type do
 
   it 'exists' do
-    skip 'Need implementation'
     expect { Nacha::Record::PosAddenda }.to_not raise_error()
   end
 
