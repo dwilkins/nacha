a2a415b6d30d43c6046a786e9f7a9ff4efab58d8
David H. Wilkins <dwilkins@conecuh.com>
2019-04-26 20:04:13 -0500

    Moving away from yml - cie_addenda

--- spec/nacha/record_types/cie_addenda_spec.rb
+++ spec/nacha/record_types/cie_addenda_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 RSpec.describe "Nacha::Record::CieAddenda", :nacha_record_type do
 
   it 'exists' do
-    skip 'Need implementation'
     expect { Nacha::Record::CieAddenda }.to_not raise_error()
   end
 
