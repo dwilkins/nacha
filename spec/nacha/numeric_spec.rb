243842b0c2047bce2663ef68d102f0296fcf966f
David H. Wilkins <dwilkins@conecuh.com>
2025-06-09 19:27:25 -0500

    Cleanup Nacha::Numeric (#8)
    Understand how op_value and value work together
    
    Add some tests

---
+++ spec/nacha/numeric_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'pry'
+require 'byebug'
+
+RSpec.describe Nacha::Numeric do
+  it 'initializes with a value' do
+    numeric = described_class.new("12345")
+    expect(numeric.to_i).to eq 12345
+  end
+
+  it 'handles string values' do
+    numeric = described_class.new("  67890  ")
+    expect(numeric.to_i).to eq 67890
+  end
+
+  it 'handles nil values' do
+    numeric = described_class.new(nil)
+    expect(numeric.to_i).to eq 0
+  end
+
+  it 'handles BigDecimal values' do
+    numeric = described_class.new(BigDecimal("12345.67"))
+    expect(numeric.to_i).to eq 12345
+  end
+
+  it 'responds to precision' do
+    numeric = described_class.new(BigDecimal(8))
+    expect(numeric.precision).to eq 1
+  end
+
+  it 'should not change spaces' do
+    numeric = described_class.new("   ")
+    expect(numeric.to_i).to eq "   "
+  end
+end
